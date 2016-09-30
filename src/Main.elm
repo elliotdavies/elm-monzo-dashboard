bbc9151c06618b69f0f34578dc3f068d91fd8721
Elliot Davies <elliot.davies@the-times.co.uk>
2016-09-30 19:09:28 +0100

    Add functionality for /accounts endpoint and refactor to abstract accordingly

--- src/Main.elm
+++ src/Main.elm
@@ -11,25 +11,41 @@ main =
         { init = init
         , update = update
         , view = view
-        , subscriptions = subscriptions
+        , subscriptions = (\_ -> Sub.none)
         }
 
 
 type alias Model =
     { authToken : Monzo.Token
-    , whoAmIData : Monzo.WhoAmI
+    , err : Maybe String
+    , whoAmIData :
+        Maybe
+            { client_id : String
+            , user_id : String
+            }
+    , accountsData :
+        Maybe
+            (List
+                { id : String
+                , created : String
+                , description : String
+                }
+            )
     }
 
 
 type Msg
-    = QueryMonzo
-    | FromMonzo Monzo.Msg
+    = QueryMonzo Monzo.Endpoint
+    | WhoAmIHandler Monzo.Msg
+    | AccountsHandler Monzo.Msg
 
 
 init : ( Model, Cmd Msg )
 init =
     ( { authToken = Monzo.auth
-      , whoAmIData = Monzo.whoAmIInit
+      , err = Nothing
+      , whoAmIData = Nothing
+      , accountsData = Nothing
       }
     , Cmd.none
     )
@@ -38,22 +54,103 @@ init =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        QueryMonzo ->
-            ( model, Cmd.map FromMonzo <| Monzo.whoAmIRequest model.authToken )
+        QueryMonzo endpoint ->
+            let
+                handler =
+                    case endpoint of
+                        Monzo.WhoAmI ->
+                            WhoAmIHandler
 
-        FromMonzo details ->
-            ( { model | whoAmIData = Monzo.whoAmIHandler details }, Cmd.none )
+                        Monzo.Accounts ->
+                            AccountsHandler
+            in
+                ( model, Cmd.map handler <| Monzo.makeApiRequest model.authToken endpoint )
 
+        WhoAmIHandler monzoMsg ->
+            case Monzo.whoAmIHandler monzoMsg of
+                Ok success ->
+                    ( { model | err = Nothing, whoAmIData = Just success }, Cmd.none )
 
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
+                Err err ->
+                    ( { model | err = Just err, whoAmIData = Nothing }, Cmd.none )
+
+        AccountsHandler monzoMsg ->
+            case Monzo.accountsHandler monzoMsg of
+                Ok success ->
+                    ( { model | err = Nothing, accountsData = Just success }, Cmd.none )
+
+                Err err ->
+                    ( { model | err = Just err, accountsData = Nothing }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ div [] [ text model.whoAmIData.client_id ]
-        , div [] [ text model.whoAmIData.user_id ]
-        , button [ onClick QueryMonzo ] [ text "Get details" ]
-        ]
+    let
+        errHtml =
+            let
+                message =
+                    case model.err of
+                        Just err ->
+                            err
+
+                        Nothing ->
+                            "No errors... yet"
+            in
+                div []
+                    [ h3 [] [ text "Errors:" ]
+                    , div [] [ text message ]
+                    ]
+
+        whoAmIHtml =
+            let
+                heading =
+                    h3 [] [ text "Who am I:" ]
+            in
+                case model.whoAmIData of
+                    Just data ->
+                        div []
+                            [ heading
+                            , div []
+                                [ text <| "client_id: " ++ data.client_id
+                                , br [] []
+                                , text <| "user_id: " ++ data.user_id
+                                ]
+                            ]
+
+                    Nothing ->
+                        div []
+                            [ heading
+                            , text "..."
+                            ]
+
+        accountsHtml =
+            let
+                heading =
+                    h3 [] [ text "Accounts:" ]
+            in
+                case model.accountsData of
+                    Just data ->
+                        div []
+                            [ heading
+                            , div [] [ text <| toString data ]
+                            , div [] [ text <| "length: " ++ (toString <| List.length data) ]
+                            ]
+
+                    Nothing ->
+                        div []
+                            [ heading
+                            , text "..."
+                            ]
+
+        buttonsHtml =
+            div []
+                [ button [ onClick <| QueryMonzo Monzo.WhoAmI ] [ text "Get details" ]
+                , button [ onClick <| QueryMonzo Monzo.Accounts ] [ text "Get accounts" ]
+                ]
+    in
+        div []
+            [ buttonsHtml
+            , errHtml
+            , whoAmIHtml
+            , accountsHtml
+            ]
--- src/Monzo.elm
+++ src/Monzo.elm
@@ -3,10 +3,10 @@ module Monzo
         ( Msg
         , auth
         , Token
-        , WhoAmI
-        , whoAmIInit
-        , whoAmIRequest
+        , Endpoint(..)
+        , makeApiRequest
         , whoAmIHandler
+        , accountsHandler
         )
 
 import Http
@@ -15,6 +15,8 @@ import Json.Decode as Json exposing ((:=))
 import Debug
 
 
+{-| Types used by this module
+-}
 type Msg
     = RequestSucceed Http.Response
     | RequestFail Http.RawError
@@ -28,13 +30,29 @@ type alias Url =
     String
 
 
+type Endpoint
+    = WhoAmI
+    | Accounts
+
+
+{-| Internal module setup
+-}
+baseUrl : String
+baseUrl =
+    "https://api.monzo.com"
+
+
+{-| Authenticates with Monzo and returns an auth token
+-}
 auth : Token
 auth =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaSI6Im9hdXRoY2xpZW50XzAwMDA5NFB2SU5ER3pUM2s2dHo4anAiLCJleHAiOjE0NzUyNTc2MDcsImlhdCI6MTQ3NTIzNjAwNywianRpIjoidG9rXzAwMDA5Q3Ezdnp1Qkh6MXhMdlhYb3YiLCJ1aSI6InVzZXJfMDAwMDk0Umd5NGJoUm55MmVURkdySiIsInYiOiIyIn0.xRBJ9J8brldCSwx9TCK4_HOzVaPC4ajvomLMViDCLfQ"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaSI6Im9hdXRoY2xpZW50XzAwMDA5NFB2SU5ER3pUM2s2dHo4anAiLCJleHAiOjE0NzUyNzkzNjksImlhdCI6MTQ3NTI1Nzc2OSwianRpIjoidG9rXzAwMDA5Q3FhSXBWcElDTHg1TG1XR24iLCJ1aSI6InVzZXJfMDAwMDk0Umd5NGJoUm55MmVURkdySiIsInYiOiIyIn0.qrANutco0c8qnEUEOcsZ5CGqcnzYMBQBKhxoM6uTeU0"
 
 
-getWithAuth : Token -> Url -> Task.Task Http.RawError Http.Response
-getWithAuth token url =
+{-| Makes a GET request with authentication
+-}
+getRequestWithAuth : Token -> Url -> Task.Task Http.RawError Http.Response
+getRequestWithAuth token url =
     Http.send Http.defaultSettings
         { verb = "GET"
         , headers = [ ( "Authorization", "Bearer " ++ token ) ]
@@ -45,57 +63,93 @@ getWithAuth token url =
 
 makeRequest : Token -> Url -> Cmd Msg
 makeRequest token url =
-    Task.perform RequestFail RequestSucceed (getWithAuth token url)
+    Task.perform RequestFail RequestSucceed (getRequestWithAuth token url)
 
 
-{-| Code related to the whoAmI method
+{-| Makes an API request to the given endpoint
 -}
-type alias WhoAmI =
+makeApiRequest : Token -> Endpoint -> Cmd Msg
+makeApiRequest token endpoint =
+    let
+        url =
+            case endpoint of
+                WhoAmI ->
+                    baseUrl ++ "/ping/whoami"
+
+                Accounts ->
+                    baseUrl ++ "/accounts"
+    in
+        makeRequest token url
+
+
+{-| Extracts either the "value" of an HTTP response from the API, or the error if the request failed
+-}
+extractValueOrError : Msg -> Result String String
+extractValueOrError msg =
+    case msg of
+        RequestSucceed data ->
+            case data.value of
+                Http.Text value ->
+                    Ok value
+
+                _ ->
+                    Debug.crash "Somehow hit Http.Blob"
+
+        RequestFail failure ->
+            Err ("Request failed: " ++ (toString failure))
+
+
+{-| Code related to the /accounts endpoint
+-}
+type alias AccountData =
+    { id : String
+    , created : String
+    , description : String
+    }
+
+
+accountsHandler : Msg -> Result String (List AccountData)
+accountsHandler msg =
+    case extractValueOrError msg of
+        Ok data ->
+            Json.decodeString accountsDecoder data
+
+        Err err ->
+            Err err
+
+
+accountsDecoder : Json.Decoder (List AccountData)
+accountsDecoder =
+    let
+        accountDecoder =
+            Json.object3 AccountData
+                ("id" := Json.string)
+                ("created" := Json.string)
+                ("description" := Json.string)
+    in
+        "accounts" := Json.list accountDecoder
+
+
+{-| Code related to the /whoami endpoint
+-}
+type alias WhoAmIData =
     { client_id : String
     , user_id : String
     }
 
 
-whoAmIInit : WhoAmI
-whoAmIInit =
-    WhoAmI "" ""
-
-
-whoAmIRequest : Token -> Cmd Msg
-whoAmIRequest token =
-    let
-        url =
-            "https://api.monzo.com/ping/whoami"
-    in
-        makeRequest token url
-
-
-whoAmIHandler : Msg -> WhoAmI
+whoAmIHandler : Msg -> Result String WhoAmIData
 whoAmIHandler msg =
-    let
-        extract value =
-            case Json.decodeString whoAmIDecoder value of
-                Ok ok ->
-                    WhoAmI ok.client_id ok.user_id
+    case extractValueOrError msg of
+        Ok data ->
+            Json.decodeString whoAmIDecoder data
 
-                Err err ->
-                    WhoAmI "e" "f"
-    in
-        case msg of
-            RequestSucceed data ->
-                case data.value of
-                    Http.Text value ->
-                        extract value
-
-                    _ ->
-                        Debug.crash "Somehow hit Http.Blob"
-
-            RequestFail fail ->
-                WhoAmI "c" "d"
+        Err err ->
+            Err err
 
 
-whoAmIDecoder : Json.Decoder WhoAmI
+whoAmIDecoder : Json.Decoder WhoAmIData
 whoAmIDecoder =
-    Json.object2 WhoAmI
+    Json.object2 WhoAmIData
         ("client_id" := Json.string)
         ("user_id" := Json.string)
